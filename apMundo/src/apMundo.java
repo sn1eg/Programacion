155a2eca0a6565fbc218de232c3af76ea0ad987c
trampitax <trampitaxonfire@gmail.com>
2018-12-01 19:57:31 +0100

    Programacion

--- apMundo/src/apMundo.java
+++ apMundo/src/apMundo.java
@@ -1,5 +1,4 @@
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -11,10 +10,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -29,15 +26,16 @@ public class apMundo {
     private static ArrayList<String> paises = new ArrayList<String>();
     private static ArrayList<String> resultados = new ArrayList<String>();
 
+//    https://www.tutorialspoint.com/java_xml/java_xpath_parse_document.htm
+//    http://chuwiki.chuidiang.org/index.php?title=Ejemplo_sencillo_de_JComboBox
+//    https://stackoverflow.com/questions/16695903/using-xml-to-populate-a-jcombobox-something-missing
+//    https://stackoverflow.com/questions/33196567/adding-java-variable-to-xpath#
 
     public apMundo() {
         iniciarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 // Boton Iniciar
-
-
-
 
                 DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
                 try {
@@ -82,6 +80,8 @@ public class apMundo {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
 //  Buscar
+
+                area_Resultados.setText("");
                 String seleccion = (String) combo_Pais.getSelectedItem();
 
 
@@ -108,7 +108,7 @@ public class apMundo {
                     }
                     System.out.println(resultados.toString());
                     Collections.sort(resultados);
-                    resultados.forEach((pais) -> area_Resultados.append(pais + "\n"));
+                    resultados.forEach((pais) -> area_Resultados.setText(area_Resultados.getText() + pais + "\n"));
 //                    resultados.forEach((pais) -> area_Resultados.set);
                 } catch (XPathExpressionException e) {
                     e.printStackTrace();
@@ -127,11 +127,7 @@ public class apMundo {
 
 
 
-    private void start() throws ParserConfigurationException, IOException, SAXException{
-
-    }
-
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+    public static void main(String[] args){
 
         JFrame frame = new JFrame("XML palabras");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -140,42 +136,6 @@ public class apMundo {
         //frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-//
-//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//        DocumentBuilder builder = factory.newDocumentBuilder();
-//
-//        File inputFile = new File("aeropuertos.xml");
-//        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-//        DocumentBuilder dBuilder;
-//
-//        dBuilder = dbFactory.newDocumentBuilder();
-//
-//        Document doc = dBuilder.parse(inputFile);
-//        doc.getDocumentElement().normalize();
-//
-//
-//        System.out.println("get");
-//        XPath xPath = XPathFactory.newInstance().newXPath();
-//        try {
-//            XPathExpression expression = xPath.compile("//aeropuerto[@pais]");
-//            NodeList nodeList = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
-//            System.out.println("nodos " + nodeList.getLength());
-//            for (int n = nodeList.getLength() - 1; n >= 0; n--) {
-//                Node nodo = nodeList.item(n);
-//                short nodeType = nodo.getNodeType();
-//                if (nodeType == Node.ELEMENT_NODE) {
-//                    paises.add(nodo.getAttributes().getNamedItem("nombreCorto").getNodeValue());
-//                }
-//            }
-//            System.out.println(paises.toString());
-//            Collections.sort(paises);
-//            paises.forEach((pais) -> System.out.println(pais));
-//        } catch (XPathExpressionException e) {
-//            e.printStackTrace();
-//        }
-
-
 
     }
 }
