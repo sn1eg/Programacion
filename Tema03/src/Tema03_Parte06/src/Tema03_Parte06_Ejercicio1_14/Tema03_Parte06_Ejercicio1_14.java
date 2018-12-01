4bf1933c23f8347082ea1f42005dc0c8ec1a590b
trampitax <trampitaxonfire@gmail.com>
2018-12-01 19:05:11 +0100

    Programacion

--- Tema03/src/Tema03_Parte06/src/Tema03_Parte06_Ejercicio1_14/Tema03_Parte06_Ejercicio1_14.java
+++ Tema03/src/Tema03_Parte06/src/Tema03_Parte06_Ejercicio1_14/Tema03_Parte06_Ejercicio1_14.java
@@ -1,4 +1,103 @@
 package Tema03_Parte06_Ejercicio1_14;
 
+import com.sun.source.doctree.SinceTree;
+
 public class Tema03_Parte06_Ejercicio1_14 {
+
+    public static boolean esCapicua(int num){
+
+        int faltante = 0;
+        int numeroInvertido = 0;
+        int restante = 0;
+
+        faltante=num;
+        numeroInvertido=0;
+        restante=0;
+
+        while(faltante!=0) {
+
+            restante=faltante%10;
+            numeroInvertido=numeroInvertido*10+restante;
+            faltante=faltante/10;
+
+        }
+
+        if(numeroInvertido==num){
+            return true;
+        }else{
+            return false;
+
+        }
+    }
+
+
+    public static boolean esPrimo(int num){
+
+        boolean primo = true;
+        for(int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                primo = false;
+                break;
+            }
+        }
+        if (primo){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static int siguientePrimo(int num){
+
+        return 0;
+    }
+
+    public static int potencia(int num){
+
+        return 0;
+    }
+
+    public static int digitos(int num){
+        String aux = "num";
+        num = aux.length();
+        return num;
+    }
+
+    public static int voltea(int num){
+        int cifra, inverso = 0;
+        while(num!=0){
+            cifra = num%10;
+            inverso = (inverso * 10) + cifra;
+            num/=10;
+        }
+        return inverso;
+    }
+
+    public static int digitoN(int n, int num){
+        String aux = "num";
+
+        num = aux.charAt(n);
+        return num;
+    }
+
+    public static int posicionDeDigito(String caracter, int num){
+        String aux = "num";
+        num = aux.indexOf(caracter);
+        return num;
+    }
+
+    public static int quitaPorDetras(int n, int num){
+        String aux = "num";
+        int longitud = aux.length();
+        String aux2 = "";
+
+        longitud = longitud-n;
+
+        for(int f = 0; f < longitud; f++){
+            //Por aqui me quede
+        }
+
+        return 0;
+    }
 }
--- apMundo/src/apMundo.java
+++ apMundo/src/apMundo.java
@@ -9,83 +9,173 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class apMundo {
     private JPanel panel_Main;
     private JLabel lbl_Pais;
-    private JComboBox combo_Pais;
+    JComboBox combo_Pais;
     private JButton btn_Buscar;
+    private JTextArea area_Resultados;
+    private JButton iniciarButton;
 
     private static ArrayList<String> paises = new ArrayList<String>();
-    private static Document xmlDoc = null;
+    private static ArrayList<String> resultados = new ArrayList<String>();
 
 
-    private void getNodos() {
-        System.out.println("get");
-        XPath xPath = XPathFactory.newInstance().newXPath();
-        try {
-            XPathExpression expression = xPath.compile("//aeropuerto[@pais='Spain']");
-            NodeList nodeList = (NodeList) expression.evaluate(xmlDoc, XPathConstants.NODESET);
-            System.out.println("nodos " + nodeList.getLength());
-            for (int n = nodeList.getLength() - 1; n >= 0; n--) {
-                Node nodo = nodeList.item(n);
-                short nodeType = nodo.getNodeType();
-                if (nodeType == Node.ELEMENT_NODE) {
-                    paises.add(nodo.getAttributes().getNamedItem("nombreCorto").getNodeValue());
+    public apMundo() {
+        iniciarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+// Boton Iniciar
+
+
+
+
+                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                try {
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    File inputFile = new File("aeropuertos.xml");
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder;
+                    dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(inputFile);
+                    doc.getDocumentElement().normalize();
+                    System.out.println("get");
+                    XPath xPath = XPathFactory.newInstance().newXPath();
+                    XPathExpression expression = xPath.compile("//aeropuerto");
+                    NodeList nodeList = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
+                    System.out.println("nodos " + nodeList.getLength());
+                    for (int n = nodeList.getLength() - 1; n >= 0; n--) {
+                        Node nodo = nodeList.item(n);
+                        short nodeType = nodo.getNodeType();
+                        if (nodeType == Node.ELEMENT_NODE) {
+                            paises.add(nodo.getAttributes().getNamedItem("pais").getNodeValue());
+                        }
+                    }
+                    System.out.println(paises.toString());
+                    Collections.sort(paises);
+
+                    for(String aux : paises){
+                        combo_Pais.addItem(aux);
+                    }
+
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (XPathExpressionException e) {
+                    e.printStackTrace();
                 }
             }
-            System.out.println(paises.toString());
-            Collections.sort(paises);
-            paises.forEach((pais) -> System.out.println(pais));
-        } catch (XPathExpressionException e) {
-            e.printStackTrace();
-        }
+        });
+        btn_Buscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+//  Buscar
+                String seleccion = (String) combo_Pais.getSelectedItem();
+
+
+                XPath xPath = XPathFactory.newInstance().newXPath();
+                try {
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    File inputFile = new File("aeropuertos.xml");
+                    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder dBuilder;
+                    dBuilder = dbFactory.newDocumentBuilder();
+                    Document doc = dBuilder.parse(inputFile);
+                    doc.getDocumentElement().normalize();
+
+                    XPathExpression expression = xPath.compile("//aeropuerto[@pais='"+seleccion+"']");
+                    NodeList nodeList = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
+                    System.out.println("nodos " + nodeList.getLength());
+                    for (int n = nodeList.getLength() - 1; n >= 0; n--) {
+                        Node nodo = nodeList.item(n);
+                        short nodeType = nodo.getNodeType();
+                        if (nodeType == Node.ELEMENT_NODE) {
+                            resultados.add(nodo.getAttributes().getNamedItem("nombreCorto").getNodeValue());
+                        }
+                    }
+                    System.out.println(resultados.toString());
+                    Collections.sort(resultados);
+                    resultados.forEach((pais) -> area_Resultados.append(pais + "\n"));
+//                    resultados.forEach((pais) -> area_Resultados.set);
+                } catch (XPathExpressionException e) {
+                    e.printStackTrace();
+                } catch (ParserConfigurationException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (SAXException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
-    public static void main(String[] args) {
 
 
 
-        /*String[] palabrasXML = new String[0];
+    private void start() throws ParserConfigurationException, IOException, SAXException{
 
-        try {
-            final String dir = System.getProperty("user.dir");
-            File inputFile = new File(dir + File.separator + "src" + File.separator + "aeropuertos.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = null;
-            try {
-                dBuilder = dbFactory.newDocumentBuilder();
-            } catch (ParserConfigurationException e1) {
-                e1.printStackTrace();
-            }
-            Document doc = null;
-            try {
-                doc = dBuilder.parse(inputFile);
-            } catch (SAXException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
-            doc.getDocumentElement().normalize();
-            System.out.println("Elemento rastaticíz :" + doc.getDocumentElement().getNodeName());
-            NodeList nodos = doc.getElementsByTagName("ROW");
-            System.out.println(nodos.getLength());
-            palabrasXML = new String[nodos.getLength()];
-            for (int i = 0; i < nodos.getLength(); i++) {
-                Node nodo = nodos.item(i);
-                if (nodo.getNodeType() == Node.ELEMENT_NODE) {
-                    Element palabra = (Element) nodo;
-                    palabrasXML[i] = palabra.getAttribute("aeuropuerto");
-                }
-            }
+    }
 
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }*/
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+
+        JFrame frame = new JFrame("XML palabras");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new apMundo().panel_Main);
+        frame.setSize(550, 300);
+        //frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+//
+//        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder builder = factory.newDocumentBuilder();
+//
+//        File inputFile = new File("aeropuertos.xml");
+//        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+//        DocumentBuilder dBuilder;
+//
+//        dBuilder = dbFactory.newDocumentBuilder();
+//
+//        Document doc = dBuilder.parse(inputFile);
+//        doc.getDocumentElement().normalize();
+//
+//
+//        System.out.println("get");
+//        XPath xPath = XPathFactory.newInstance().newXPath();
+//        try {
+//            XPathExpression expression = xPath.compile("//aeropuerto[@pais]");
+//            NodeList nodeList = (NodeList) expression.evaluate(doc, XPathConstants.NODESET);
+//            System.out.println("nodos " + nodeList.getLength());
+//            for (int n = nodeList.getLength() - 1; n >= 0; n--) {
+//                Node nodo = nodeList.item(n);
+//                short nodeType = nodo.getNodeType();
+//                if (nodeType == Node.ELEMENT_NODE) {
+//                    paises.add(nodo.getAttributes().getNamedItem("nombreCorto").getNodeValue());
+//                }
+//            }
+//            System.out.println(paises.toString());
+//            Collections.sort(paises);
+//            paises.forEach((pais) -> System.out.println(pais));
+//        } catch (XPathExpressionException e) {
+//            e.printStackTrace();
+//        }
+
+
+
     }
 }
