496cddfda87c1e52b4d37d1caa4cb3b673623a24
trampitax <trampitaxonfire@gmail.com>
2018-12-04 20:44:53 +0100

    Programacion

---
+++ apMundoD v2.0/src/aeropuerto.java
@@ -0,0 +1,23 @@
+public class aeropuerto {
+
+    double latitud;
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    double longitud;
+
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+}
