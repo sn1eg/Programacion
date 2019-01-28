ab0b26a86536cf0562f1189cfd9792f902884201
trampitax <trampitaxonfire@gmail.com>
2019-01-28 14:40:46 +0100

    PProg Style

---
+++ demoAL/src/com/codebind/votos.java
@@ -0,0 +1,51 @@
+package com.codebind;
+
+public class votos {
+    String name;
+    int votos;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+
+    public int getUltvoto() {
+        return ultvoto;
+    }
+
+    public void setUltvoto(int ultvoto) {
+        this.ultvoto = ultvoto;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    int ultvoto;
+    String dia;
+    String hora;
+
+}
